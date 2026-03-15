bb012a2642b4531fac19513be47844d8e2f7a847
Lokesh Pinninti <lpinninti149@gmail.com>
2026-03-15 15:05:16 +0530

    add tests and deployment phases

---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int a = 2 + 2;
+
+    if(a == 4) {
+        printf("Test Passed\n");
+        return 0;
+    }
+    else {
+        printf("Test Failed\n");
+        return 1;
+    }
+
+}
